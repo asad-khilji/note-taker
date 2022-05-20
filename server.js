7a4a8305e1146ca58112dde09e28ebcd19f727d5
Asad Khilji <khiljiasad2@gmail.com>
2022-05-20 16:35:09 -0400

    added database id

--- server.js
+++ server.js
@@ -23,6 +23,30 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(staticPath, "notes.html"));
 })
 
+//Setting routes for APIs
+//This gets notes saved and joins it in db.json
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"))
+});
+
+// Post function to add new notes to db.json
+app.post("/api/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const newNotes = req.body;
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    res.json(notes);
+});
+
+//used for deleting notes
+app.delete("/api/notes/:id", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const delNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
+    fs.writeFileSync("./db/db.json", JSON.stringify(delNote));
+    res.json(delNote);
+})
+
 app.listen(3000, () => {
     console.log('listening on port 3000......');
 })
